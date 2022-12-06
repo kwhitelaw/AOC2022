4b8526bf1f06d5000fcd20011f47f89472dd09ea
kwhitelaw <whitelaw.kenneth@gmail.com>
2022-12-05 20:13:18 -0500

    Add files via upload

--- AOC2022-Day4pt2.py
+++ AOC2022-Day4pt2.py
@@ -1,4 +1,10 @@
-#Day 4 part 1
+#Day 4 part 2
+
+def between(alpha,bravo,charlie):
+    result = bool(0)
+    if bravo >= alpha and bravo <= charlie:
+        result = bool(1)
+    return result
 
 file_object = open(r'C:\Users\whitelawk\Desktop\KJW\Advent 2022\day4input.txt')
 assignments = file_object.read().splitlines()
@@ -15,14 +21,8 @@ for assignment in assignments:
     elf_1_end = int(assignment_list[elf_1].split('-')[1])
     elf_2_start = int(assignment_list[elf_2].split('-')[0])
     elf_2_end = int(assignment_list[elf_2].split('-')[1])
-    if (elf_1_start <= elf_2_start and elf_1_end >= elf_2_end) or (elf_1_start >= elf_2_start and elf_1_end <= elf_2_end):
-        counter += 1
-        print ("fully contained, counter = "+str(counter))
-        print (str(elf_1_start)+'-'+str(elf_1_end)+"   "+str(elf_2_start)+'-'+str(elf_2_end))
-        
+    if between(elf_1_start,elf_2_start,elf_1_end) or between(elf_1_start,elf_2_end,elf_1_end) or between(elf_2_start,elf_1_start,elf_2_end) or between(elf_2_start,elf_1_end,elf_2_end):
+        counter +=1
 
 print(counter)
-    
-    
-    
     
